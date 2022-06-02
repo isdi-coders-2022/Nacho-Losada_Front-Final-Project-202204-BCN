d6833c4f896a7e03529a84ec948d4817b9239b01
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-06-02 13:22:01 +0200

    Removed unused import in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import LoginForm from "./components/LoginForm/LoginForm";
-import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
   return (
