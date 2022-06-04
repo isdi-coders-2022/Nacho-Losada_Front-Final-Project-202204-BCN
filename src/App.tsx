87ebd1114a1c4b882435a6220a83444a14c3d88c
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-06-04 19:22:36 +0200

    Added user feedbacvk with toats. Added custom styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import LoadingModal from "./components/LoadingModal/LoadingModal";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+function App(): JSX.Element {
   return (
     <>
+      <LoadingModal />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
