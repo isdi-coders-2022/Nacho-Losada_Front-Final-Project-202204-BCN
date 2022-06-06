146485aa783af2fb0f0242a3c00befaf96c5b1db
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-06-06 10:49:26 +0200

    Added a summoner mock

---
+++ src/mocks/mockSummoner.ts
@@ -0,0 +1,14 @@
+const mockSummoner = {
+  summonerName: "Abraham The Door",
+  creatorName: "Abra",
+  rankImage:
+    "http://ddragon.leagueoflegends.com/cdn/12.10.1/img/champion/Aatrox.png",
+  rank: "Gold IV",
+  firstRole: "/images/positions/Bot.png",
+  firstRoleChamps: ["Zac", "Annie", "Graves"],
+  secondRole: "/images/positions/Top.png",
+  secondRoleChamps: ["Zac", "Annie", "Graves"],
+  description: "Lolen Ipsum",
+};
+
+export default mockSummoner;
