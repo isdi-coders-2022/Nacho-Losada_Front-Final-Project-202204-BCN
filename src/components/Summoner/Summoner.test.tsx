da923a5f237113e7a21bf1ab78566c3bd9c19bf3
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-06-07 16:34:21 +0200

    Tested summonersPage

--- src/components/Summoner/Summoner.test.tsx
+++ src/components/Summoner/Summoner.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import mockSummoner from "../../mocks/mockSummoner";
+import { mockSummoner } from "../../mocks/mocks";
 import Summoner from "./Summoner";
 
 describe("Given a Summoner component", () => {
