b44f4b43fbcf2cafacfef8cf5c9be1f9f8a41a6b
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-06-14 22:53:16 +0200

    Created slice to get ownsummoners

---
+++ src/redux/features/ownSummonersSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ISummoner } from "../../components/Summoner/Summoner";
+
+const initialState: ISummoner[] = [];
+
+const ownSummonersSlice = createSlice({
+  name: "ownSummoners",
+  initialState,
+  reducers: {
+    loadOwnList: (ownSummoners, action: PayloadAction<ISummoner[]>) =>
+      action.payload,
+  },
+});
+
+export const { loadOwnList: loadOwnListActionCreator } =
+  ownSummonersSlice.actions;
+export default ownSummonersSlice.reducer;
